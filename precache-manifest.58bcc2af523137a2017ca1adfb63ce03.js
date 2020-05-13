fd10c47246dcdba3d3c5a441c62ab0202e584889
harman099 <61110096+harman099@users.noreply.github.com>
2020-05-13 11:19:16 +0530

    Updates

---
+++ precache-manifest.58bcc2af523137a2017ca1adfb63ce03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "510168ba3537754fb269674f0c52fbf6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d91edc1744624d2e5394",
+    "url": "/monsters-rolodex/static/css/main.b1796348.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d91edc1744624d2e5394",
+    "url": "/monsters-rolodex/static/js/main.e6387133.chunk.js"
+  },
+  {
+    "revision": "e3f9453037e5f28f8167",
+    "url": "/monsters-rolodex/static/js/runtime-main.7c0f1f0f.js"
+  }
+]);
